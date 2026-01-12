3a68ed0749d90da54082afd71384c130e5f907ff
Amy Lee Gaerty <gaertyamy@gmail.com>
2026-01-12 11:50:12 +0000

    Fixed duplicate category name error

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -22,7 +22,7 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255'
+            'name' => 'required|string|max:255|unique:categories,name'
         ]);
 
         Category::create([
